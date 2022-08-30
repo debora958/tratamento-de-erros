e8a692464b3f2d6ac32b724b72bbd429696a178e
debora958 <109834177+debora958@users.noreply.github.com>
2022-08-30 15:11:34 -0300

    Add files via upload

---
+++ excecao.js
@@ -0,0 +1,12 @@
+    let num = 1
+    try {
+        num.toUpperCase();
+    }
+    catch (error) {
+        console.log('name', error.name);
+        console.log('message', error.message);
+    }
+
+
+
+ 
